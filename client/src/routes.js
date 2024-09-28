6f65239cab68452ada6d34a7c516b41f9ac9a06b
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2024-09-28 17:03:36 -0400

    moved template

---
+++ client/src/routes.js
@@ -0,0 +1,37 @@
+// import
+import React, { Component }  from 'react';
+import Dashboard from "views/Dashboard/Dashboard.js";
+import Billing from "views/Dashboard/Billing.js";
+import Profile from "views/Dashboard/Profile.js";
+import SignIn from "views/Pages/SignIn.js";
+import SignUp from "views/Pages/SignUp.js";
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+  DocumentIcon,
+  RocketIcon,
+  SupportIcon,
+} from "components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: <HomeIcon color='inherit' />,
+    component: Dashboard,
+    layout: "/admin",
+  },
+  // {
+  //   path: "/billing",
+  //   name: "Billing",
+  //   rtlName: "لوحة القيادة",
+  //   icon: <CreditIcon color='inherit' />,
+  //   component: Billing,
+  //   layout: "/admin",
+  // },
+];
+export default dashRoutes;
